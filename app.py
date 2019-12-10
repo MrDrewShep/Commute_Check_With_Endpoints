5f474b439da4cad77082409c3ff90851c8510cf7
user name <user@user.com>
2019-12-10 10:12:21 -0500

    Web form complete

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
 from flask import Flask, request, jsonify
 import json
-from pprint import pprint
 
 app = Flask(__name__)
 
 @app.route('/test', methods=["POST"])
 def prelim():
-    google_response = json.loads(request.form["google_response"])
-    print(google_response["routes"][0]["legs"][0]["distance"])
+    form = request.form
     return {
         "message": "hit the endpoint",
-        "data": google_response
+        "data": form
     }
 
 if __name__ == "__main__":
